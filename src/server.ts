aa16c9971537709834ba689d86df5712b1485eb1
oluwatobi <oluwatobiisaiah0409@gmail.com>
2024-05-09 23:16:28 +0100

    updating serverless deployment..

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import app from "./app.js";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import config from "./config/config.js";
 (async () => {
     try {
-       const connection = await mongoose.connect(config.DATABASE_URI);
+      //  const connection = await mongoose.connect(config.DATABASE_URI);
        app.listen(config.APP_PORT, () => {
           console.log(`🚀 Server ready at ${config.APP_PORT}`);
        });
